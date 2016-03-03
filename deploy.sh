3744318d9f9b6fcd5e73bd3e7b4e085aa0a0ae0d
billvsme <994171686@qq.com>
2016-03-03 23:30:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
-sudo apt-get install git
-sudo apt-get install vim
-sudo apt-get install tmux
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt-get install -y git
+sudo apt-get install -y vim
+sudo apt-get install -y tmux
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
 cp .tmux.conf ~/
 vim +PluginInstall +qall
