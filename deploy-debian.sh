d4f1f65f3a9624e2ce8a9121af69da4d88e9c387
billvsme <994171686@qq.com>
2016-10-19 23:50:25 +0800

    Mv config file to config dir

--- deploy-debian.sh
+++ deploy-debian.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get install -y wget
 sudo apt-get install -y git
 sudo apt-get install -y vim
 sudo apt-get install -y tmux
 sudo apt-get install -y zsh
+
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
-cp .tmux.conf ~/
-cp .zshrc ~/
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+cp config/.vimrc ~/
+cp config/.tmux.conf ~/
+cp config/.zshrc ~/
+
 vim +PluginInstall +qall
+
 chsh -s /bin/zsh
--- deploy-redhat.sh
+++ deploy-redhat.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 sudo yum -y install epel-release
 sudo yum -y install wget
 sudo yum -y install git
 sudo yum -y install vim
 sudo yum -y install tmux
 sudo yum -y install zsh
+
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
-cp .tmux.conf ~/
-cp .zshrc ~/
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+cp config/.vimrc ~/
+cp config/.tmux.conf ~/
+cp config/.zshrc ~/
+
 vim +PluginInstall +qall
+
 chsh -s /bin/zsh
