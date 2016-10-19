95e3802c53dd9b02d4f62cf32ebec642bffb848a
billvsme <994171686@qq.com>
2016-10-19 23:36:37 +0800

    deploy.sh add zsh

--- deploy-redhat.sh
+++ deploy-redhat.sh
@@ -4,9 +4,11 @@ sudo yum -y install wget
 sudo yum -y install git
 sudo yum -y install vim
 sudo yum -y install tmux
+sudo yum -y install zsh
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
 cp .tmux.conf ~/
 cp .zshrc ~/
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 vim +PluginInstall +qall
+chsh -s /bin/zsh
